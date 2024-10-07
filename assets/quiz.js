799958e646430071fa291a345c1dd2ae988e9056
Craig Conrad <craig@blackanvildigital.com>
2024-10-06 20:43:39 -0500

    quiz branch 10/6 20:42CDT - dynamically rendered questions/answers

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,24 +1,66 @@
-const finish_btn = document.getElementById("finish-button");
 
-const quiz_form = document.querySelector("#quizForm");
+const quizForm = document.querySelector("#quizForm");
 const answers = {};
 
-quiz_form.addEventListener("change", updateAnswers);
+quizForm.addEventListener("change", updateAnswers);
 function updateAnswers(event) {
   const key = event.target.name;
   const value = event.target.value;
   answers[key] = value;
-
-
-
-  console.log("answers: ", answers);
 }
 
+let currentQuestionIndex = 0;
+const previousBtn = document.getElementById("prev-button");
+const nextBtn = document.getElementById("next-button");
+const finishBtn = document.getElementById("finish-button");
 
-finish_btn.addEventListener("click", function () {
+function updateButtonStates(index) {
+    previousBtn.disabled = (index === 0)
+    nextBtn.disabled = (index === questions.length - 1);
+    nextBtn.classList.toggle("hidden", index === questions.length - 1);
+    finishBtn.classList.toggle("hidden", index !== questions.length - 1);
+}
+
+previousBtn.addEventListener("click", (event) => {
+    currentQuestionIndex--;
+    updateButtonStates(currentQuestionIndex);
+    displayQuestion(currentQuestionIndex);
+});
+
+nextBtn.addEventListener("click", (event) => {
+    currentQuestionIndex++;
+    updateButtonStates(currentQuestionIndex);
+    displayQuestion(currentQuestionIndex);
+});
+
+finishBtn.addEventListener("click", function () {
   if (answers.product_type && answers.bike_type && answers.color) {
     alert("Here are your answers: " + JSON.stringify(answers));
   } else {
     alert("Please answer all the questions!");
   }
 });
+
+function displayQuestion(index) {
+    const currentQuestion = questions[index];
+    const currentAnswer = answers[currentQuestion.name];
+    quizForm.innerHTML = `
+    <h2>${index + 1}) ${currentQuestion.text}</h2>
+    <div class="label-container">
+        ${currentQuestion.options.map((option) =>
+            `
+                <input type="radio" id="${option.value}" name="${currentQuestion.name}" value="${option.value}" ${option.value === currentAnswer ? 'checked' : ''}>
+                <label for="${option.value}">
+                    <div class="img-wrap">
+                        <img src="${option.img}"/>
+                    </div>
+                    <span>${option.label}</span>
+                </label>
+            `
+        ).join('')}
+    `
+}
+
+// initialization
+displayQuestion(currentQuestionIndex);
+updateButtonStates(currentQuestionIndex);

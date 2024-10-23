347600f6bafecf0e0f1aeb0940356ccea571f664
Craig Conrad <craig@blackanvildigital.com>
2024-10-23 00:14:57 -0500

    testimonials slider branch 10/23 00:14CDT - initialize all sliders w/ dynamic settings

--- assets/custom-testimonials-slider.js
+++ assets/custom-testimonials-slider.js
@@ -8,10 +8,23 @@
 // the slides or cells should be aligned to the left side on initialization.
 // pauseAutoPlayOnHover: false
 
-const testimonialSlider = new Flickity('.testimonial-slider', {
-    cellAlign: 'left',
-    wrapAround: true,
-    pageDots: false,
-    autoPlay: false,
-    pauseAutoPlayOnHover: false
+// const testimonialSlider = new Flickity('.testimonial-slider', {
+//     cellAlign: 'left',
+//     wrapAround: true,
+//     pageDots: false,
+//     autoPlay: false,
+//     pauseAutoPlayOnHover: false
+// });
+
+const testimonialSliders = document.querySelectorAll('.testimonial-slider');
+testimonialSliders.forEach((slider) => {
+    const autoPlay = Number(slider.dataset.autoplay);
+    new Flickity(slider, {
+        cellAlign: 'left',
+        wrapAround: true,
+        pageDots: false,
+        autoPlay: autoPlay,
+        pauseAutoPlayOnHover: false
+    })
 });
+
